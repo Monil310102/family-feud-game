1e8049e522cd2f0aac437833949d80c711233899
Monil310102 <monilhiteshkumar@gmail.com>
2025-12-18 13:14:42 +0000

    Added game interfaces

---
+++ src/types/games.ts
@@ -0,0 +1,24 @@
+export interface Answer {
+    text: string;
+    points: number;
+    isRevealed: boolean;
+}
+
+export interface Question {
+    questionText: string;
+    answers: Answer[];
+}
+
+export interface GameState {
+    team1Score: number;
+    team2Score: number;
+    currentQuestionIndex: number;
+    currentRoundIndex: number;
+    questions: Question[];
+    strikeCount: number;
+    isGameActive: boolean;
+    revealedAnswersCount: number;
+    activeTeam: 'team1' | 'team2';
+    currentBank : number;
+
+}
